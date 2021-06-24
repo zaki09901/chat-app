ea4e91e50373772d97987458a361d91027ef48d0
zaki09901 <85482210+zaki09901@users.noreply.github.com>
2021-06-24 15:42:03 +0900

    ユーザー情報表示の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_24_053835) do
+ActiveRecord::Schema.define(version: 2021_06_24_061801) do
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
